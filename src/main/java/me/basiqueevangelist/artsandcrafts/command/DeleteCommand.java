9cae093f28684c08d725d810e80061939adc067b
Basique <basiqueevangelist@yandex.ru>
2024-02-06 19:48:23 +0300

    add support for deleting haircut of entity

--- src/main/java/me/basiqueevangelist/artsandcrafts/command/DeleteCommand.java
+++ src/main/java/me/basiqueevangelist/artsandcrafts/command/DeleteCommand.java
@@ -5,19 +5,24 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import me.basiqueevangelist.artsandcrafts.cca.ArtsAndCraftsCCA;
 import me.basiqueevangelist.artsandcrafts.haircut.HaircutsState;
 import me.basiqueevangelist.artsandcrafts.item.ArtsAndCraftsItems;
 import me.basiqueevangelist.artsandcrafts.item.TemplateItem;
 import me.lucko.fabric.api.permissions.v0.Permissions;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Util;
 
+import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public final class DeleteCommand {
     private static final SimpleCommandExceptionType NO_HAIRCUT_HELD = new SimpleCommandExceptionType(Text.translatable("error.artsandcrafts.noHaircutHeld"));
+    private static final SimpleCommandExceptionType NO_HAIRCUT_TARGET = new SimpleCommandExceptionType(Text.translatable("error.artsandcrafts.noHaircutTarget"));
 
     private DeleteCommand() {
 
@@ -27,10 +32,46 @@ public final class DeleteCommand {
         dispatcher.register(literal("artsandcrafts")
             .then(literal("delete")
                 .requires(Permissions.require("artsandcrafts.deleteHaircut", 3))
-                .executes(DeleteCommand::delete)));
+                .then(literal("inhand")
+                    .executes(DeleteCommand::deleteInHand))
+                .then(literal("target")
+                    .then(argument("target", EntityArgumentType.entity())
+                        .executes(DeleteCommand::deleteTarget)))));
     }
 
-    private static int delete(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+    private static int deleteTarget(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+        var entity = EntityArgumentType.getEntity(ctx, "target");
+        var component = entity.getComponent(ArtsAndCraftsCCA.HAIRCUT);
+
+        if (component.haircutId().equals(Util.NIL_UUID))
+            throw NO_HAIRCUT_TARGET.create();
+
+        var state = HaircutsState.get(ctx.getSource().getServer());
+        var haircut = state.haircuts().get(component.haircutId());
+
+        if (haircut == null)
+            throw NO_HAIRCUT_TARGET.create();
+
+        state.deleteHaircut(haircut);
+
+        String owner = ctx.getSource().getServer()
+            .getUserCache()
+            .getByUuid(haircut.ownerId())
+            .map(GameProfile::getName)
+            .orElseGet(() -> haircut.ownerId().toString());
+
+        // TODO: translate.
+        ctx.getSource().sendFeedback(() -> Text.literal("Deleted ")
+            .append(Text.literal(haircut.name())
+                .formatted(Formatting.GRAY))
+            .append(Text.literal(" by "))
+            .append(Text.literal(owner)
+                .formatted(Formatting.AQUA)), true);
+
+        return 1;
+    }
+
+    private static int deleteInHand(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
         var player = ctx.getSource().getPlayerOrThrow();
         var handStack = player.getStackInHand(Hand.MAIN_HAND);
 
